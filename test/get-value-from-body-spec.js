19b55daf189c336a4e3ad147c9a9f7cd42c4d626
Max Lettenberger <maxlettenberger@gmail.com>
2020-11-13 13:51:43 -0800

    Finish save catego

--- test/get-value-from-body-spec.js
+++ test/get-value-from-body-spec.js
@@ -26,12 +26,13 @@ describe("The getValueFromBody function", () => {
     // Act
     // Write code to invoke getValueFromBody and collect
     // the result
+    let result = getValueFromBody(body, key);
 
 
     // Assert
     // Replace the fail line with an assertion for the
     // expected value of ""
-    expect.fail('please write this test');
+    expect(result).to.equal("")
   });
 
   it('returns the value of the key in a simple body', () => {
@@ -41,27 +42,27 @@ describe("The getValueFromBody function", () => {
     // Act
     // Write code to invoke getValueFromBody and collect
     // the result
-
+    let result = getValueFromBody(body, key);
 
     // Assert
     // Replace the fail line with an assertion for the
     // expected value of "Bess"
-    expect.fail('please write this test');
+    expect(result).to.equal("Bess")
   });
 
   it('returns the value of the key in a complex body', () => {
     const body = "name=Bess&age=29&job=Boss";
     // Select one of the keys in the body
-
+    const key = "age";
     // Act
     // Write code to invoke getValueFromBody and collect
     // the result
-
+    let result = getValueFromBody(body, key);
 
     // Assert
     // Replace the fail line with an assertion for the
     // expected value for the key that you selected
-    expect.fail('please write this test');
+    expect(result).to.equal('29')
   });
 
   it('decodes the return value of URL encoding', () => {
@@ -71,12 +72,12 @@ describe("The getValueFromBody function", () => {
     // Act
     // Write code to invoke getValueFromBody and collect
     // the result
-
+    let result = getValueFromBody(body, key);
 
     // Assert
     // Replace the fail line with an assertion for the
     // expected value for the key that you selected that
     // has the encoded value in it
-    expect.fail('please write this test');
+    expect(result).to.equal('Level Thirty-One')
   });
 });
